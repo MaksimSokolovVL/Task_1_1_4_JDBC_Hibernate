0a7b6cf01611f7127ae225c289d3ecf22e2a89ec
MaksimSokolovVL <volokos2010@gmail.com>
2023-03-01 01:26:42 +0300

    cleaned up the code UserDaoJDBCImpl.java

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
-    private final Statement statement;
+
     private final Connection connection;
 
     public UserDaoJDBCImpl() {
@@ -17,68 +17,48 @@ public class UserDaoJDBCImpl implements UserDao {
             String USER = "root";
             String PASSWORD = "x0HuDgA5L2df";
             connection = DriverManager.getConnection(SERVER_MYSQL_URL, USER, PASSWORD);
-            statement = connection.createStatement();
         } catch (SQLException exception) {
             throw new RuntimeException(exception);
         }
     }
 
     public void createUsersTable() {
-        try {
-            String createTableSQL = """
-                    CREATE TABLE IF NOT EXISTS public.users (
-                    id BIGINT NOT NULL AUTO_INCREMENT,
-                    last_name VARCHAR(45) NOT NULL,
-                    second_name VARCHAR(45) NOT NULL,
-                    age TINYINT NOT NULL,
-                    PRIMARY KEY (`id`))
-                    ENGINE = InnoDB;""";
-            statement.executeUpdate(createTableSQL);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        executeUpdate("""
+                CREATE TABLE IF NOT EXISTS public.users (
+                id BIGINT NOT NULL AUTO_INCREMENT,
+                last_name VARCHAR(45) NOT NULL,
+                second_name VARCHAR(45) NOT NULL,
+                age TINYINT NOT NULL,
+                PRIMARY KEY (`id`))
+                ENGINE = InnoDB;""");
     }
 
     public void dropUsersTable() {
-        try {
-            String dropTableSQL = "DROP TABLE IF EXISTS users;";
-            statement.executeUpdate(dropTableSQL);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        executeUpdate("DROP TABLE IF EXISTS users;");
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        try {
-            String SAVE_USER = """
-                    INSERT INTO public.users (last_name, second_name, age)
-                    VALUES ('%s', '%s', %d);""";
-            statement.executeUpdate(String.format(SAVE_USER, name, lastName, age));
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-
+        String SAVE_USER = """
+                INSERT INTO public.users (last_name, second_name, age)
+                VALUES ('%s', '%s', %d);""";
+        executeUpdate(String.format(SAVE_USER, name, lastName, age));
     }
 
     public void removeUserById(long id) {
-        try {
-            String DELETE_USER = """
-                    DELETE
-                    FROM public.users
-                    WHERE id = %d;""";
-            statement.executeUpdate(String.format(DELETE_USER, id));
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-
+        String DELETE_USER = """
+                DELETE
+                FROM public.users
+                WHERE id = %d;""";
+        executeUpdate(String.format(DELETE_USER, id));
     }
 
     public List<User> getAllUsers() {
         List<User> userList = new ArrayList<>();
 
-        try {
+        try (Statement statement = connection.createStatement()) {
             String SELECT_FROM_PUBLIC_USERS = "SELECT * FROM public.users;";
             ResultSet resultSet = statement.executeQuery(SELECT_FROM_PUBLIC_USERS);
+
             while (resultSet.next()) {
                 userList.add(new User(
                                 resultSet.getString("last_name"),
@@ -94,10 +74,12 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void cleanUsersTable() {
-        try {
-            String CLEAN_TABLE = "TRUNCATE TABLE public.users;";
-            statement.executeUpdate(CLEAN_TABLE);
-            System.out.println("Чисто");
+        executeUpdate("TRUNCATE TABLE public.users;");
+    }
+
+    private void executeUpdate(String sql) {
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate(sql);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
