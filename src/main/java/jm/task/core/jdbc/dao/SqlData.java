dba9f5ed6ba8711e335180c121a9eb7392614fd3
MaksimSokolovVL <volokos2010@gmail.com>
2023-03-01 12:57:50 +0300

    create new branch, add data class SqlData.java and connection class ConnectionData.java
    so I changed the logic in the class UserDaoJDBCImpl.java

---
+++ src/main/java/jm/task/core/jdbc/dao/SqlData.java
@@ -0,0 +1,27 @@
+package jm.task.core.jdbc.dao;
+
+public class SqlData {
+
+    private SqlData() {
+    }
+
+    public static final String CREATE_USER_TAB = """
+            CREATE TABLE IF NOT EXISTS public.users (
+            id BIGINT NOT NULL AUTO_INCREMENT,
+            last_name VARCHAR(45) NOT NULL,
+            second_name VARCHAR(45) NOT NULL,
+            age TINYINT NOT NULL,
+            PRIMARY KEY (`id`))
+            ENGINE = InnoDB;""";
+
+    public static final String DROP_USERS_TAB = "DROP TABLE IF EXISTS users;";
+    public static final String ADD_USER = """
+            INSERT INTO public.users (last_name, second_name, age)
+            VALUES ('%s', '%s', %d);""";
+    public static final String DELETE_USER = """
+            DELETE
+            FROM public.users
+            WHERE id = %d;""";
+    public static final String SELECT_FROM_PUBLIC_USERS = "SELECT * FROM public.users;";
+    public static final String CLEAN_USERS_TAB = "TRUNCATE TABLE public.users;";
+}
--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -8,56 +8,30 @@ import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
 
-    private final Connection connection;
-
     public UserDaoJDBCImpl() {
-
-        try {
-            String SERVER_MYSQL_URL = "jdbc:mysql://80.78.246.73:3306/public";
-            String USER = "root";
-            String PASSWORD = "x0HuDgA5L2df";
-            connection = DriverManager.getConnection(SERVER_MYSQL_URL, USER, PASSWORD);
-        } catch (SQLException exception) {
-            throw new RuntimeException(exception);
-        }
     }
 
     public void createUsersTable() {
-        executeUpdate("""
-                CREATE TABLE IF NOT EXISTS public.users (
-                id BIGINT NOT NULL AUTO_INCREMENT,
-                last_name VARCHAR(45) NOT NULL,
-                second_name VARCHAR(45) NOT NULL,
-                age TINYINT NOT NULL,
-                PRIMARY KEY (`id`))
-                ENGINE = InnoDB;""");
+        executeUpdate(SqlData.CREATE_USER_TAB);
     }
 
     public void dropUsersTable() {
-        executeUpdate("DROP TABLE IF EXISTS users;");
+        executeUpdate(SqlData.DROP_USERS_TAB);
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        String SAVE_USER = """
-                INSERT INTO public.users (last_name, second_name, age)
-                VALUES ('%s', '%s', %d);""";
-        executeUpdate(String.format(SAVE_USER, name, lastName, age));
+        executeUpdate(String.format(SqlData.ADD_USER, name, lastName, age));
     }
 
     public void removeUserById(long id) {
-        String DELETE_USER = """
-                DELETE
-                FROM public.users
-                WHERE id = %d;""";
-        executeUpdate(String.format(DELETE_USER, id));
+        executeUpdate(String.format(SqlData.DELETE_USER, id));
     }
 
     public List<User> getAllUsers() {
         List<User> userList = new ArrayList<>();
 
-        try (Statement statement = connection.createStatement()) {
-            String SELECT_FROM_PUBLIC_USERS = "SELECT * FROM public.users;";
-            ResultSet resultSet = statement.executeQuery(SELECT_FROM_PUBLIC_USERS);
+        try (Statement statement = ConnectionData.getConnection().createStatement()) {
+            ResultSet resultSet = statement.executeQuery(SqlData.SELECT_FROM_PUBLIC_USERS);
 
             while (resultSet.next()) {
                 userList.add(new User(
@@ -74,11 +48,11 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void cleanUsersTable() {
-        executeUpdate("TRUNCATE TABLE public.users;");
+        executeUpdate(SqlData.CLEAN_USERS_TAB);
     }
 
     private void executeUpdate(String sql) {
-        try (Statement statement = connection.createStatement()) {
+        try (Statement statement = ConnectionData.getConnection().createStatement()) {
             statement.executeUpdate(sql);
         } catch (SQLException e) {
             throw new RuntimeException(e);
